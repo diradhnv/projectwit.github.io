6d513b6f6893e4ac0e54c59912b8dfd6c20c823e
diradhnv <105736979+diradhnv@users.noreply.github.com>
2022-08-03 14:16:48 +0700

    Delete 2_Cybersecurity.py

--- pages/2_Cybersecurity.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-
-st.header("Cyber Security")
-st.subheader("Pengertian")
-st.write("Cyber security adalah tindakan yang dilakukan untuk melindungi sistem komputer dari serangan atau akses ilegal. Mengacu dari International Telecommunications Union (ITU), tindakan cyber security ini mencakup alat, kebijakan, konsep keamanan, dll, yang dapat digunakan untuk melindungi organisasi, dan aset pengguna. Perlindungan terhadap organisasi dan aset pengguna termasuk perangkat komputasi, aplikasi, layanan dan informasi yang dikirimkan dan / atau disimpan di lingkungan cyber. Dengan menerapkan cyber security, Anda dapat mengurangi resiko ancaman yang menyasar sistem komputer Anda.")
